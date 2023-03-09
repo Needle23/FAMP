1121e3edfdaedb1c1f905de718f3e90a5b4c41b8
Needle <63046732+Needle23@users.noreply.github.com>
2023-03-09 12:53:45 +0200

    Update install_serv.sh

--- install_serv.sh
+++ install_serv.sh
@@ -53,14 +53,46 @@ pkg install -y apache24
 sysrc apache24_enable="YES"
 
 # Install MariaDB 10.6
-pkg install -y mariadb105-server mariadb105-client
+pkg install -y mariadb106-server mariadb106-client
 
 # Add service to be fired up at boot time
 sysrc mysql_enable="YES"
 sysrc mysql_args="--bind-address=127.0.0.1"
 
-# Install PHP 8.2 and its 'funny' dependencies
-pkg install -y php82 php82-mysqli php82-mbstring php82-zlib php82-curl php82-gd php82-json php82-composer php82-extensions php82-hash php82-session php82-pdo_mysql
+pkg install -y	php82\
+		php82-bcmath\
+		php82-bz2\
+		php82-ctype\
+		php82-curl\
+		php82-dom\
+		php82-exif\
+		php82-extensions\
+		php82-fileinfo\
+		php82-filter\
+		php82-ftp\
+		php82-gd\
+		php82-iconv\
+		php82-intl\
+		php82-mbstring\
+		php82-mysqli\
+		php82-opcache\
+		php82-pdo\
+		php82-pdo_mysql\
+		php82-pecl-mcrypt\
+		php82-phar\
+		php82-posix\
+		php82-session\
+		php82-simplexml\
+		php82-soap\
+		php82-sockets\
+		php82-sqlite3\
+		php82-tokenizer\
+		php82-xml\
+		php82-xmlreader\
+		php82-xmlwriter\
+		php82-zip\
+		php82-zlib
+
 
 # Install the 'old fashioned' Expect to automate the mysql_secure_installation part
 pkg install -y expect
